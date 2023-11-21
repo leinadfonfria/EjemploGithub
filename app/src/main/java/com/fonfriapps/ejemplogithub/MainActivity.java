e878596d4871f8a4dbaa8fa6169aa29382a34c25
Daniel Rodriguez Aira <leinadfonfria@gmail.com>
2023-11-21 23:31:36 +0100

    Proyecto android vacío

---
+++ app/src/main/java/com/fonfriapps/ejemplogithub/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fonfriapps.ejemplogithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
